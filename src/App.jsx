2d28a4798c0fd907a0ed6a7f1a51939d64e2cf90
Mouhamad-Shaker-1 <mouhamadshaker073@gmail.com>
2024-02-04 11:00:28 -0600

    the game

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import {useState, useEffect} from 'react'
+import Die from "./components/Die.jsx"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+
+function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  const [numberRoll, setNumberRoll] = useState(0)
+  const [time, setTime] = useState(new Date())
+  
+  useEffect(() => {
+      const allHeld = dice.every(die => die.isHeld)
+      const firstValue = dice[0].value
+      const allSameValue = dice.every(die => die.value === firstValue)
+      if (allHeld && allSameValue) {
+          setTenzies(true)
+      }
+  }, [dice])
+
+  function generateNewDie() {
+      return {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
+      }
+  }
+  
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(generateNewDie())
+      }
+      return newDice
+  }
+  
+  function rollDice() {
+      if(!tenzies) {
+          setDice(oldDice => oldDice.map(die => {
+              return die.isHeld ? 
+                  die :
+                  generateNewDie()
+          }))
+          setNumberRoll(prevNumber => prevNumber + 1)
+      } else {
+          setTenzies(false)
+          setDice(allNewDice())
+          setNumberRoll(0)
+          setTime(new Date())
+      }
+  }
+
+  
+  function holdDice(id) {
+      setDice(oldDice => oldDice.map(die => {
+          return die.id === id ? 
+              {...die, isHeld: !die.isHeld} :
+              die
+      }))
+  }
+  
+  const diceElements = dice.map(die => (
+      <Die 
+          key={die.id} 
+          value={die.value} 
+          isHeld={die.isHeld} 
+          holdDice={() => holdDice(die.id)}
+      />
+  ))
+
+  function theTime() {
+
+    let endDate   = new Date();
+    let seconds = (endDate.getTime() - time.getTime()) / 1000;
+
+    console.log(seconds)
+    return seconds
+  }
+
+
+
+
+
+  return (
+      <main onClick={theTime}>
+          {tenzies && <Confetti />}
+          <h1 className='title'>Tenzies</h1>
+          <p className='instructions'>Roll until all dice are the same. 
+          Click each die to freeze it at its current value between rolls.</p>
+          <div className='diceContainer'>
+              {diceElements}
+          </div>
+          <button 
+              className='rollDice' 
+              onClick={rollDice}
+          >
+              {tenzies ? "New Game" : "Roll"}
+          </button>
+          { tenzies &&
+              <>
+                  <p>the number of roll is {numberRoll}</p>
+                  <p>you take {theTime()} second time to complent the roll</p>
+              </>
+          }
+      </main>
+  )
+}
+
+export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Die(props) {
+    // const stylesHold = {
+    //     backgroundColor: props.isHeld ? "#59E391" : "white"
+    // }
+   
+    const colorDice = props.isHeld ? "blackDice" : "witheDice"
+    const numberDotes = props.value
+
+    return (
+        <div 
+            className='dieFace' 
+            // style={stylesHold}
+            onClick={props.holdDice}
+        >
+            <img className='imgDice' src={`../img/${colorDice}/diceDice (${numberDotes}).png`} />
+            <p className='number'>{numberDotes}</p>
+            {/* <h2 className={styles.dieNum}>{props.value}</h2> */}
+        </div>
+    )
+}
